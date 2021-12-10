b491541dfed809fe20a455f55cbcfb774e2c179d
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-12-10 11:37:03 +0900

    Splash

---
+++ app/src/main/java/com/example/after_corona/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.after_corona.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/after_corona/view/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.example.after_corona.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.databinding.DataBindingUtil
+import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivityMainBinding
+import com.example.after_corona.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+}
