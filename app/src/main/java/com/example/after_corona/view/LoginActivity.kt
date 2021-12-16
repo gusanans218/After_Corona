86f15a0a8fbb25767582a6a5309a6078ab449e39
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-12-16 23:22:16 +0900

    [CREATE] Login

--- app/src/main/java/com/example/after_corona/view/LoginActivity.kt
+++ app/src/main/java/com/example/after_corona/view/LoginActivity.kt
@@ -4,27 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivityLoginBinding
 import com.example.after_corona.databinding.ActivitySignupBinding
 
-class SignupActivity : AppCompatActivity() {
-    private val binding by lazy { ActivitySignupBinding.inflate(layoutInflater) }
+class LoginActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.activity = this
 
-        binding.signupBtn.setOnClickListener {
-               val intent = Intent(this, Signup2Activity::class.java)
-                startActivity(intent)
-            }
-
-        binding.yesaccount.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
+        binding.loginNoaccount.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
-        }
 
         }
     }
-
-
-
+}
--- app/src/main/java/com/example/after_corona/view/Signup2Activity.kt
+++ app/src/main/java/com/example/after_corona/view/Signup2Activity.kt
@@ -1,12 +1,27 @@
 package com.example.after_corona.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivitySignup2Binding
+import com.example.after_corona.databinding.ActivitySignupBinding
 
 class Signup2Activity : AppCompatActivity() {
+    private val binding by lazy { ActivitySignup2Binding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup2)
+        setContentView(binding.root)
+        binding.activity = this
+
+        binding.signupBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.yesaccount2.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
