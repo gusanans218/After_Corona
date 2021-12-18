c7118b9aeaa0a8bf38eaee4ad85ee1e9c6adec1c
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-12-18 22:54:21 +0900

    [create]Dialog

--- app/src/main/java/com/example/after_corona/view/DeviceLinkageActivity.kt
+++ app/src/main/java/com/example/after_corona/view/DeviceLinkageActivity.kt
@@ -2,11 +2,30 @@ package com.example.after_corona.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivityDeviceLinkageBinding
+import com.example.after_corona.databinding.ActivityLoginBinding
 
-class DeviceLinkageActivity : AppCompatActivity() {
+class DeviceLinkageActivity : AppCompatActivity(),View.OnClickListener {
+    private val binding by lazy { ActivityDeviceLinkageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_device_linkage)
+        setContentView(binding.root)
+        binding.activity = this
+
+
+        binding.deviceBtn.setOnClickListener (this)
+
+    }
+
+
+    override fun onClick(v: View) {
+        when(v?.id) {
+            binding.deviceBtn.id -> {
+                val dlg = DeviceDialog(this)
+                dlg.show()
+            }
+        }
     }
 }
