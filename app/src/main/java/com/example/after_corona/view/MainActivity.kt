44a5939ac84619ab46e60a0f6bbf54dba052c224
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-12-21 17:32:25 +0900

    [CREATE] bottom navi

--- app/src/main/java/com/example/after_corona/view/MainActivity.kt
+++ app/src/main/java/com/example/after_corona/view/MainActivity.kt
@@ -3,13 +3,53 @@ package com.example.after_corona.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.after_corona.R
+import com.example.after_corona.databinding.ActivityLinkageSuccessBinding
 import com.example.after_corona.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+        binding.activity = this
+
+
+        binding.vp.apply { adapter = ViewPagerAdapter(this@MainActivity)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bn.selectedItemId = when (position) {
+                        0 -> R.id.menu_calendar
+                        1 -> R.id.menu_home
+                        else -> R.id.menu_user
+                    }
+                }
+            })
+        }
+        binding.bn.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_calendar -> binding.vp.currentItem = 0
+                R.id.menu_home -> binding.vp.currentItem = 1
+                else -> binding.vp.currentItem = 2 }
+            true
+        }
+    }
+    inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount() = 3
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> CalendarFragment()
+                1 -> HomeFragment()
+                else -> UserFragment()
+            }
+        }
     }
 }
+
+
