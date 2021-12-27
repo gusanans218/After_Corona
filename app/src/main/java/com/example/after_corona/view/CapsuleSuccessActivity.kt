cca3e6840321a8a817b6f29575c91bc8384b56a1
SeungHyun <67039023+gusanans218@users.noreply.github.com>
2021-12-27 20:43:09 +0900

    [CREATE] CapsuleSuccessActivity 구현

---
+++ app/src/main/java/com/example/after_corona/view/CapsuleSuccessActivity.kt
@@ -0,0 +1,12 @@
+package com.example.after_corona.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.after_corona.R
+
+class CapsuleSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capsule_success)
+    }
+}
